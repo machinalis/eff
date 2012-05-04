41ee7087061451aa042b59577bc84695646db2aa
Alejandro Peralta <aperalta@machinalis.com>
2012-05-04 14:14:24 -0300

    removed unused db configuration

--- eff_site/settings.py
+++ eff_site/settings.py
@@ -32,9 +32,9 @@ MANAGERS = ADMINS
 # DATABASE_ENGINE = 'sqlite3'   # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 # DATABASE_NAME = 'eff.db'      # Or path to database file if using sqlite3.
 DATABASE_ENGINE = 'postgresql_psycopg2'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'machinalis_eff'           # Or path to database file if using sqlite3.
-DATABASE_USER = 'machinalis_eff'           # Not used with sqlite3.
-DATABASE_PASSWORD = 'machinalis_eff'          # Not used with sqlite3.
+DATABASE_NAME = ''           # Or path to database file if using sqlite3.
+DATABASE_USER = ''           # Not used with sqlite3.
+DATABASE_PASSWORD = ''          # Not used with sqlite3.
 DATABASE_HOST = ''              # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''              # Set to empty string for default. Not used with sqlite3.
 
