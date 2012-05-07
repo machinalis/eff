258234fa2ed75aaec789256c12457cfc588925e8
Juan Norris <jnorris@machinalis.com>
2012-05-07 15:51:58 -0300

    Cambios para correr localmente

--- eff_site/settings.py
+++ eff_site/settings.py
@@ -29,14 +29,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# DATABASE_ENGINE = 'sqlite3'   # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-# DATABASE_NAME = 'eff.db'      # Or path to database file if using sqlite3.
-DATABASE_ENGINE = 'postgresql_psycopg2'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'machinalis_eff'           # Or path to database file if using sqlite3.
-DATABASE_USER = 'machinalis_eff'           # Not used with sqlite3.
-DATABASE_PASSWORD = 'machinalis_eff'          # Not used with sqlite3.
-DATABASE_HOST = ''              # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''              # Set to empty string for default. Not used with sqlite3.
+# DATABASE_ENGINE = 'sqlite3' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+# DATABASE_NAME = 'eff.db' # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = '' # Or path to database file if using sqlite3.
+DATABASE_USER = '' # Not used with sqlite3.
+DATABASE_PASSWORD = '' # Not used with sqlite3.
+DATABASE_HOST = '' # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = '' # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -134,4 +134,3 @@ try:
     from local_settings import *
 except ImportError:
     pass
-
