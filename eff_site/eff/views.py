2295fcddf11b1d87c6acba8b0aa2e166962be4d2
Juan Norris <jnorris@machinalis.com>
2012-06-14 16:01:49 -0300

    Fixing #138 (A user can see logged hours of users being followed

--- eff_site/eff/views.py
+++ eff_site/eff/views.py
@@ -171,20 +171,27 @@ def __enough_perms(u):
 
 def __enough_perms_or_follows(view_fun):
     def _dec(request, user_name):
-        # req_user can access this view if he has required permissions or
-        # if he is following user, or if req_user = user
-        req_user = request.user
-        req_up = req_user.get_profile()
+        logged_user = request.user
+        logged_up = logged_user.get_profile()
         user = User.objects.get(username=user_name)
-        check = ((req_user.has_perm('eff.view_billable') and
-                 req_user.has_perm('eff.view_wage')) or
-                 user in req_up.watches.all() or
-                 user == req_user)
 
-        if check:
-            return view_fun(request, user_name)
+        # If logged_user has required permissions he has full access to reports.
+        # otherwise, he can only access detailed reports (which means he can not
+        # see reports involving money), and he can only see reports for himself
+        # and users being followed by him.
+        if not (logged_user.has_perm('eff.view_billable') and \
+               logged_user.has_perm('eff.view_wage')):
+            if (user in logged_up.watches.all()) or (user == logged_user):
+                if ('export' in request.GET and not 'detailed' in request.GET):
+                    return HttpResponseRedirect('/accounts/login/?next=%s' %
+                                                quote(request.get_full_path()))
+                else:
+                    return view_fun(request, user_name)
+            else:
+                return HttpResponseRedirect('/accounts/login/?next=%s' %
+                                            quote(request.get_full_path()))
         else:
-            return redirect('login')
+            return view_fun(request, user_name)
 
     return _dec
 
@@ -652,18 +659,18 @@ def eff_charts(request):
 def eff_report(request, user_name):
 
     context = __process_dates(request)
-    context['export_allowed'] = True
+    # context['export_allowed'] = True
     # if not (request.user.has_perm('eff.view_billable') and \
     #         request.user.has_perm('eff.view_wage')):
     #     if request.user.username != user_name:
     #         return HttpResponseRedirect('/accounts/login/?next=%s' % quote(
     #             request.get_full_path()))
     #     else:
-    if 'export' in request.GET:
-        return HttpResponseRedirect('/accounts/login/?next=%s' % quote(
-            request.get_full_path()))
-    else:
-        del context['export_allowed']
+    # if 'export' in request.GET:
+    #     return HttpResponseRedirect('/accounts/login/?next=%s' % quote(
+    #         request.get_full_path()))
+    # else:
+    #     del context['export_allowed']
 
     from_date = context['from_date']
     to_date = context['to_date']
