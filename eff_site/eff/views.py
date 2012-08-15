e49bbc91589e3edd89c52cd2bb82e6d8df9bfb88
Juan Norris <jnorris@machinalis.com>
2012-08-15 14:06:09 -0300

    Added an import that was missing.

--- eff_site/eff/views.py
+++ eff_site/eff/views.py
@@ -24,6 +24,7 @@ import urlparse
 import operator
 
 from urllib import quote, urlencode
+from subprocess import Popen
 from datetime import date, timedelta, datetime
 
 from django.shortcuts import render_to_response, get_object_or_404, redirect
